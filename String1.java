fdae792fc694c8fe216ecb8201500439581ebd8c
Sandeep Kumar <114137033+Sandy-365@users.noreply.github.com>
2024-02-02 02:12:40 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class String1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the height of shape ::>> ");
+        int height = scanner.nextInt();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = height; k > i; k--) {
+                System.out.print(" #");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
