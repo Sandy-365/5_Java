8fe485adf724e49b17fce7d5ed73c61d2b956739
Sandeep Kumar <114137033+Sandy-365@users.noreply.github.com>
2024-02-03 20:07:26 +0530

    Create BMI.java

---
+++ BMI.java
@@ -0,0 +1,21 @@
+/*
+ * take i/p of weight in(kgs) and height(m) of the user
+ * return wwther the user is obese , fit or skinny according to the BMI
+ *  bmi < 18 -> skinny
+ *  bmi >= 18 && bmi <= 22 -> fit
+ *  else obese
+ */
+
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       double w = sc.nextDouble();
+       double h = sc.nextDouble();
+       sc.close();
+       double bmi = w/(h*h);
+       String ans = (bmi >= 18 ? (bmi <= 22 ? "Fit" : "obese") : "Skinny");
+       System.out.println("According to ur BMI you are "+ans); 
+    }
+}
