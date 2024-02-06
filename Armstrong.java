a239731d71677693331c6f21e75b6d50908ebdfd
Sandeep Kumar <114137033+Sandy-365@users.noreply.github.com>
2024-02-06 21:17:16 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ArmStrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        String num3 = sc.nextLine();
+        int length= num3.length();
+        int num=Integer.parseInt(num3);
+        int temp = num;
+        int m=0;
+        while(temp>0){
+            int b=temp%10;
+            m=m+ (int) Math.pow(b,length);
+            temp=temp/10;
+        }
+        if (m==num){
+            System.out.println("Armstrong Number");
+        }else{
+            System.out.println("Not Armstrong");
+        }
+        sc.close();
+    }
+}
